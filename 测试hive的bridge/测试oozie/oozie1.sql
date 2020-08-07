85762cb654593a29d6c338e0f6d95b078a988d55
yangchengkai <yangchengkai@yunzhangfang.com>
2020-08-07 10:34:47 +0800

    init

--- 测试hive的bridge/测试oozie/oozie1.sql
+++
@@ -1,4 +0,0 @@
--- 通过hue从hdfs倒入
--- ods_companyinfo_ent_financing_information
--- ods 插入历史表
-insert into test_atlas.companyinfo_ent_financing_information select * from test_atlas.ods_companyinfo_ent_financing_information
